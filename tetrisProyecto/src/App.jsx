63628f173998152e5ca79f1170773be6df6c8cb5
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-01-15 22:42:06 +0100

    Configura despliegue continuo en Vercel

--- tetrisProyecto/src/App.jsx
+++ tetrisProyecto/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center text-primary">Holaaa soy prueba</h1>
+      <h1 className="text-center text-primary">Holaaa soy prueba Vercel</h1>
       <button className="btn btn-success">tikitik</button>
     </div>
   );
