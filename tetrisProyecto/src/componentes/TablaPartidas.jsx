aa0899b3dc9bffb5bca69ca083585e5a6d36b118
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-01-15 23:07:09 +0100

    Define array de partidas con datos de prueba

--- tetrisProyecto/src/componentes/TablaPartidas.jsx
+++ tetrisProyecto/src/componentes/TablaPartidas.jsx
@@ -1,3 +1,5 @@
+// src/components/TablaPartidas.jsx
+
 import React, { useState } from "react";
 
 // Array inicial de las partidas
@@ -9,78 +11,18 @@ const arrayPartidasInicial = [
   { nick: "Jugador5", puntos: 1350, fecha: "2024-12-05" },
 ];
 
-// Función para ordenar partidas 
-export function ordenarPartidas(array, columna, orden = "asc") {
-  return [...array].sort((a, b) => {
-    if (columna === "puntos" || columna === "fecha") {
-      return orden === "asc"
-        ? a[columna] > b[columna]
-          ? 1
-          : -1
-        : a[columna] < b[columna]
-        ? 1
-        : -1;
-    }
-    return 0;
-  });
-}
-
-// Componente principal
 export function TablaPartidas() {
   const [partidas, setPartidas] = useState(arrayPartidasInicial);
-
-  const manejarOrden = (columna, orden) => {
-    const partidasOrdenadas = ordenarPartidas(partidas, columna, orden);
-    setPartidas(partidasOrdenadas);
-  };
-
-  const agregarPartida = () => {
-    const nuevaPartida = {
-      nick: `Jugador${partidas.length + 1}`,
-      puntos: Math.floor(Math.random() * 2000), // Puntos aleatorios
-      fecha: new Date().toISOString().split("T")[0], // Fecha actual
-    };
-    setPartidas([...partidas, nuevaPartida]);
-  };
 
   return (
     <div className="container-fluid">
       <h2 className="mt-4 mb-5">Tabla de Partidas</h2>
-      <button onClick={agregarPartida}>Agregar Partida</button>
       <table className="table table-striped">
         <thead>
           <tr>
             <th>Nick</th>
-            <th>
-              Puntos
-              <button
-                className="btn btn-link"
-                onClick={() => manejarOrden("puntos", "asc")}
-              >
-                ↑
-              </button>
-              <button
-                className="btn btn-link"
-                onClick={() => manejarOrden("puntos", "desc")}
-              >
-                ↓
-              </button>
-            </th>
-            <th>
-              Fecha
-              <button
-                className="btn btn-link"
-                onClick={() => manejarOrden("fecha", "asc")}
-              >
-                ↑
-              </button>
-              <button
-                className="btn btn-link"
-                onClick={() => manejarOrden("fecha", "desc")}
-              >
-                ↓
-              </button>
-            </th>
+            <th>Puntos</th>
+            <th>Fecha</th>
           </tr>
         </thead>
         <tbody>
