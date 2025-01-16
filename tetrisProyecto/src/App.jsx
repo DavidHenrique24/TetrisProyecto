56e3bdeb5e27d9ad98a7b099c3d1569fefe1ab81
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-01-16 01:33:01 +0100

    .

--- tetrisProyecto/src/App.jsx
+++ tetrisProyecto/src/App.jsx
@@ -1,10 +1,13 @@
 import VistaInicio from "./vistas/VistaInicio.jsx";
 import "./index.css"
+import TablaPartidas from "./vistas/Partidas.jsx";
 function App() {
   return (
     <div className="App">
-      
+       
+     
       <VistaInicio />
+      <TablaPartidas />
 
     </div>
   );
