35d0b583c27b857804a5c40c141aa844cbe44c1d
DavidHenrique24 <henriquemaciasdavidalejandro@fpllefia.com>
2025-01-21 20:28:27 +0100

    actu panel

---
+++ tetrisProyecto/src/componentes/Panel.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Panel = ({ arrayCasillas }) => {
+    return (
+      <div>
+        {arrayCasillas.map((fila, idFila) => (
+       
+        ))}
+      </div>
+    );
+  };
+  
+  export default Panel;
